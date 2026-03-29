e2b73591120b753404038fd158ee75f719c50e98
wss~ <3066646702@qq.com~>
2026-03-29 13:04:33 +0800

    feat: 开发统计页面

--- app/(tabs)/statistics.tsx
+++ app/(tabs)/statistics.tsx
@@ -1,6 +1,7 @@
 import { ThemedText } from "@/components/themed-text";
 import { ThemedView } from "@/components/themed-view";
 import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { ProgressBar } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 export default function Statistics() {
   return (
@@ -11,19 +12,36 @@ export default function Statistics() {
       </ThemedView>
       <ScrollView>
         <View className="flex-row p-5 rounded">
-          <View className="flex-1 mr-5 bg-red-400 rounded">
-            <Text>总收入</Text>
-            <Text>总收入</Text>
+          <View className="flex-1 bg-green-400 text-white rounded-xl p-4 shadow-md mr-2">
+            <Text className="text-sm text-white opacity-90">总收入</Text>
+            <Text className="text-2xl text-white">¥总收入</Text>
           </View>
-          <View className="flex-1 bg-blue-400 rounded">
-            <Text>总支出</Text>
-            <Text>总支出</Text>
+          <View className="flex-1 bg-red-400 text-white rounded-xl p-4 shadow-md">
+            <Text className="text-sm text-white opacity-90">总支出</Text>
+            <Text className="text-2xl text-white">¥总支出</Text>
           </View>
         </View>
-        <View>
-          <Text>收入</Text>
-          <Text>收入</Text>
-        </View>
+         <View className="bg-white m-5 rounded-xl p-4 shadow-sm border border-gray-200 mb-6">
+            <Text className="text-sm text-gray-600 opacity-90">本月结余</Text>
+            <Text className="text-2xl text-gray-600">¥本月结余</Text>
+          </View>
+         <View className="bg-white m-5 rounded-xl p-4 shadow-sm border border-gray-200 mb-6">
+            <Text className="text-sm text-gray-600 opacity-90 font-medium text-3xl mb-2">支出分布</Text>
+            <View className="text-2xl text-gray-600 ">
+              <View className="flex-row justify-between">
+                <Text>丈夫</Text>
+                <Text>¥100</Text>
+              </View>
+            </View>
+            <ProgressBar progress={0.5}  className="mb-4" />
+            <View className="text-2xl text-gray-600">
+              <View className="flex-row justify-between">
+                <Text>妻子</Text>
+                <Text>¥100</Text>
+              </View>
+            </View>
+            <ProgressBar progress={0.5} />
+          </View>
       </ScrollView>
     </SafeAreaView>
   );
